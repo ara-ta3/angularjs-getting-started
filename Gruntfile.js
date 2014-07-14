fc53555ee955b073bbe4a9a2b2188ca4306ad554
Arata <tarata43@yahoo.co.jp>
2014-07-14 22:27:21 +0900

    gruntでlivereload入れた

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        watch:{
+            options:{
+                livereload:true
+            },
+            files:'*.html'
+        },
+        connect:{
+            server:{
+                options:{
+                    port:8080,
+                    hostname:'127.0.0.1'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+
+    grunt.registerTask('default', ['connect','watch']);
+};
