62a29457fe28928ae5367d89273eb49bf09de69e
Arata <tarata43@yahoo.co.jp>
2014-07-14 23:04:25 +0900

    フォームの基本を学ぼう

--- myscript.js
+++ myscript.js
@@ -1,24 +1,4 @@
 var mainCtrl = function($scope) {
-    $scope.users = [
-        {"name":"taguchi","score": 100.22},
-        {"name":"tanaka","score": 200.2},
-        {"name":"sasaki","score": 50.213},
-        {"name":"hoge","score": 100.22},
-        {"name":"fuga","score": 200.2},
-        {"name":"sasaki","score": 50.213},
-        {"name":"taguchi","score": 100.22},
-        {"name":"tanaka","score": 200.2},
-        {"name":"sasaki","score": 50.213},
-        {"name":"taguchi","score": 100.22},
-        {"name":"tanaka","score": 200.2},
-        {"name":"sasaki","score": 50.213},
-
-    ];
-};
-
-// コントローラーがネストすると変数を継承できる
-var userItemCtrl = function($scope) {
-    $scope.increment = function() {
-        $scope.user.score++;
+    $scope.addUser = function(){
     }
 };
