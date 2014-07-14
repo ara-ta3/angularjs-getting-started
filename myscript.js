24ccf48b0e4a2bb0648e6c67c0906b798e9355fd
Arata <tarata43@yahoo.co.jp>
2014-07-14 22:34:45 +0900

    Controller #3

---
+++ myscript.js
@@ -0,0 +1,8 @@
+var mainCtrl = function($scope) {
+
+    $scope.users = [
+        {"name":"taguchi","score": 100},
+        {"name":"tanaka","score": 200},
+        {"name":"sasaki","score": 50},
+    ];
+}
