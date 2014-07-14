1c6f153e3112298bd2fed7c31031cacd38baa419
Arata <tarata43@yahoo.co.jp>
2014-07-14 22:43:48 +0900

    ループで使えるフィルタ #5

--- myscript.js
+++ myscript.js
@@ -1,8 +1,17 @@
 var mainCtrl = function($scope) {
-
     $scope.users = [
-        {"name":"taguchi","score": 100},
-        {"name":"tanaka","score": 200},
-        {"name":"sasaki","score": 50},
+        {"name":"taguchi","score": 100.22},
+        {"name":"tanaka","score": 200.2},
+        {"name":"sasaki","score": 50.213},
+        {"name":"hoge","score": 100.22},
+        {"name":"fuga","score": 200.2},
+        {"name":"sasaki","score": 50.213},
+        {"name":"taguchi","score": 100.22},
+        {"name":"tanaka","score": 200.2},
+        {"name":"sasaki","score": 50.213},
+        {"name":"taguchi","score": 100.22},
+        {"name":"tanaka","score": 200.2},
+        {"name":"sasaki","score": 50.213},
+
     ];
 }
